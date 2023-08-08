e7d28d1222e27714cd94898a2347f7cc977f6525
SmeagolWorms4 <smeagolworms4@gmail.com>
2023-08-08 13:08:05 +0200

    Fix entry point

--- docker/entrypoint.browser.sh
+++ docker/entrypoint.browser.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -n "$INITIAL_CONFIG" ] && [ ! -f $CONFIG_PATH/setting.json ]; then
-    echo "$INITIAL_CONFIG" > $CONFIG_PATH/setting.json
+if [ -n "$INITIAL_CONFIG" ] && [ ! -f $CONFIG_PATH/settings.json ]; then
+    echo "$INITIAL_CONFIG" > $CONFIG_PATH/settings.json
 fi
 exec "$@"
